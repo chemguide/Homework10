1cab0edd4159202317011aaef25b3719f19420ee
chemguide <argentum198@gmail.com>
2024-04-22 05:02:03 +0500

    Check HW pls

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        System.out.println("Задача 1");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + ' ' + middleName + ' ' + lastName;
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+        //Задача 2
+        System.out.println("\nЗадача 2");
+        String fullNameUpCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullNameUpCase);
+        //Задача 3
+        System.out.println("\nЗадача 3");
+        fullName = "Иванов Семён Семёнович";
+        String fullNameReplacedYo = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullNameReplacedYo);
     }
 }
